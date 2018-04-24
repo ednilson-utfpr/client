ac6ad24e84809b18879f37f30d294530d4a05d9c
vomislei <vomisleibalena@hotmail.com>
2018-04-23 22:46:36 -0300

    Perfil

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {PerfilService} from './perfil.service';
+import {Perfil} from './perfil';
+
+@Component({
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent implements OnInit {
+
+  perfils: Perfil[];
+  showDialog = false;
+  perfilEdit = new Perfil();
+
+  constructor(private perfilService: PerfilService) {
+  }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.perfilService.findAll().subscribe(e => this.perfils = e);
+  }
+
+  novo() {
+    this.showDialog = true;
+    this.perfilEdit = new Perfil();
+  }
+
+  salvar() {
+    this.perfilService.save(this.perfilEdit).subscribe(e => {
+      this.perfilEdit = new Perfil();
+      this.findAll();
+      this.showDialog = false;
+    });
+  }
+
+  editar(perfil: Perfil) {
+    this.perfilEdit = perfil;
+    this.showDialog = true;
+  }
+
+  remover(perfil: Perfil) {
+    this.perfilService.delete(perfil.id).subscribe(() => {
+      this.findAll();
+    });
+  }
+}
