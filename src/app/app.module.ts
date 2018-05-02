5aafdfa177d702eab872feabaacf8c51b57d00ee
aline <alinenichelle@hotmail.com>
2018-05-02 15:17:14 -0300

    setor e cargo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,9 @@ import {AppRouting} from './app-routing.module';
 import {LoginModule} from './login/login.module';
 import {HomeModule} from './home/home.module';
 import {ProdutoModule} from './produto/produto.module';
-<<<<<<< HEAD
 import { SetorModule } from './setor/setor.module';
 import { CargoModule } from './cargo/cargo.module';
 
-=======
-import {PerfilModule} from './perfil/perfil.module';
->>>>>>> e0aa5fd441e2f653cab97f1afb2ff81a17998f8e
 
 @NgModule({
   declarations: [
@@ -28,14 +24,9 @@ import {PerfilModule} from './perfil/perfil.module';
     HttpClientModule,
     LoginModule,
     HomeModule,
-<<<<<<< HEAD
     ProdutoModule, 
     SetorModule,
     CargoModule   
-=======
-    ProdutoModule,
-    PerfilModule
->>>>>>> e0aa5fd441e2f653cab97f1afb2ff81a17998f8e
   ],
   providers: [
     {
